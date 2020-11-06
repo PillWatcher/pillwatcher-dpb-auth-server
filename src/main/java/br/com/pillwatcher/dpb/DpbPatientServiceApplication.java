0eddec667dfca5e3628a9cfe7b49ff4e20f84251
Amanda Pires <amandoca_779@hotmail.com>
2020-11-06 20:46:50 -0300

    :sparkles: Initial structure
    Co-authored-by: filypsdias <lipao.dias@hotmail.com>
    Co-authored-by: KahCosta <kamillacosta.unb@gmail.com>
    Co-authored-by: gabrielademoraes <gabrielademoraes10@gmail.com>
    Co-authored-by: geovanne97 <geovannessaraiva97@gmail.com>

---
+++ src/main/java/br/com/pillwatcher/dpb/DpbPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.pillwatcher.dpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpbPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpbPatientServiceApplication.class, args);
+	}
+
+}
